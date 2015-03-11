815ba922320b2314f39ff99946466050a4306b77
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-03-11 19:39:06 +0100

    + chapter 1 of architecture tutorial

--- Elm/ArchitectureTutorial/1/Counter.elm
+++ Elm/ArchitectureTutorial/1/Counter.elm
@@ -1,3 +1,9 @@
+-- Counter.elm
+-- A simple intercative counter app
+--
+-- vim: ft=elm sw=2 ts=2
+--
+
 module Counter where
 
 import Html (..)
@@ -8,11 +14,54 @@ import Signal
 
 -- MODEL
 
+type alias Model = Int
+
 
 --UPDATE
+
+type Action = Increment | Decrement
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Increment -> model + 1
+    Decrement -> model - 1
 
 
 -- VIEW
 
+view : Model -> Html
+view model =
+  div []
+  [ button [ onClick (Signal.send actionChannel Decrement) ] [ text "-" ]
+  , div [ countStyle ] [ text (toString model) ]
+  , button [ onClick (Signal.send actionChannel Increment) ] [ text "+" ]
+  ]
+
+countStyle : Attribute
+countStyle =
+  style
+  [ ("font-size", "20px")
+  , ("font-family", "monospace")
+  , ("display", "inline-block")
+  , ("width", "50px")
+  , ("text-align", "center")
+  ]
+
 
 -- SIGNALS
+
+main : Signal Html
+main =
+  Signal.map view model
+
+model : Signal Model
+model =
+  Signal.foldp update 0 (Signal.subscribe actionChannel)
+
+actionChannel : Signal.Channel Action
+actionChannel =
+  Signal.channel Increment
+
+
+
