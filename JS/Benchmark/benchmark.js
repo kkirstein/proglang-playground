52b3a384ce85cbe787ef74f38ab29da638428211
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-04-16 14:34:14 +0200

    + perfect numbers

--- JS/Benchmark/benchmark.js
+++ JS/Benchmark/benchmark.js
@@ -5,25 +5,34 @@
 //
 
 var fib = require("./fib");
+var pn = require("./perfect_numbers")
 
 console.log("Fibonacci numbers:");
 console.log("------------------");
 
 var tic = new Date();
-var fib_1 = fib.fib_naive(35);
+var res = fib.fib_naive(35);
 var toc = new Date();
-console.log("fib_naive(35) = ", fib_1, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
+console.log("fib_naive(35) = ", res, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
 
 tic = new Date();
-var fib_2 = fib.fib(35);
+res = fib.fib(35);
 toc = new Date();
-console.log("fib(35) = ", fib_2, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
+console.log("fib(35) = ", res, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
 
 tic = new Date();
-var fib_3 = fib.fib(1000);
+res = fib.fib(1000);
 toc = new Date();
-console.log("fib(1000) = ", fib_3, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
+console.log("fib(1000) = ", res, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
 
+
+console.log("Perfect numbers:");
+console.log("----------------");
+
+tic = new Date();
+res = pn.perfect_numbers(10000);
+toc = new Date();
+console.log("perfect_numbers(10000) = ", res, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
 
 
 console.log("------------------");
---
+++ JS/Benchmark/perfect_numbers.js
@@ -0,0 +1,22 @@
+// perfect_numbers.js
+// Calculate perfect numbers in JavaScript
+//
+// vim: ft=javascript sw=4 ts=4
+//
+
+function isperfect(n) {
+	var sum = 0;
+	for (var i = 0; i < n; i++) {
+		if ((n % i) == 0) { sum += i; }
+	}
+	return (sum == n)
+}
+
+exports.perfect_numbers = function (n) {
+	var res = [];
+	for (var i = 1; i < n; i++) {
+		if (isperfect(i)) { res.push(i); }
+	}
+	return res
+}
+
