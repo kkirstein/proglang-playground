35f01af925c95b865a5ddde761f9730dbe9d2b79
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-04-24 16:37:49 +0200

    + Fibonacci benchmark

---
+++ Haskell/src/Benchmark/Benchmark.hs
@@ -0,0 +1,57 @@
+-- Benchmark.hs
+-- A set of (micro-) benchmarks for the Haskell
+-- programming language
+--
+-- vim: ft=haskell sw=2 ts=2 et
+--
+
+module Main where
+
+import System.CPUTime
+
+import Fibonacci as Fib
+
+-- a helper fun for timing measurement
+timeIt :: (Fractional c) => (a -> IO b) -> a -> IO c
+timeIt action arg =
+  do startTime <- getCPUTime
+     action arg
+     finishTime <- getCPUTime
+     return $ fromIntegral (finishTime - startTime) / 1000000000000
+ 
+toMSec :: (Integral a, Fractional c) => a -> a -> c
+toMSec tic toc =
+  fromIntegral (toc -tic) / 1000000000
+
+--timeIt' :: (Fractional c) => (a -> b) -> a -> IO (b, c)
+--timeIt' f arg =
+--  do tic <- getCPUTime
+--     res <- f arg
+--     toc <- getCPUTime
+--     return (res, fromIntegral $ (toc - tic) / 1000000000000)
+
+
+-- main entry point
+main :: IO ()
+main =
+  do putStrLn "Haskell Benchmarks"
+     putStrLn "=================="
+     putStrLn ""
+     putStrLn "Fibonacci numbers:"
+     putStrLn "------------------"
+     tic <- getCPUTime
+     putStrLn $ "fibNaive(35) = " ++ show (fibNaive 35)
+     toc <- getCPUTime
+     putStrLn $ "Elapsed: " ++ show (toMSec tic toc) ++ "msec."
+     tic <- getCPUTime
+     putStrLn $ "fib(35) = " ++ show (fib 35)
+     toc <- getCPUTime
+     putStrLn $ "Elapsed: " ++ show (toMSec tic toc) ++ "msec."
+     tic <- getCPUTime
+     putStrLn $ "fib(1000) = " ++ show (fib 1000)
+     toc <- getCPUTime
+     putStrLn $ "Elapsed: " ++ show (toMSec tic toc) ++ "msec."
+
+     putStrLn ""
+     putStrLn "Done!"
+
---
+++ Haskell/src/Benchmark/Fibonacci.hs
@@ -0,0 +1,22 @@
+-- Fibonacci.hs
+-- Caluclate Fibonacci numbers in Haskell
+--
+-- vim: ft=haskell sw=2 ts= et
+--
+
+module Fibonacci where
+
+
+-- naive approach
+fibNaive :: Integer -> Integer
+fibNaive n
+  | n < 2 = n
+  | otherwise = fibNaive (n -1) + fibNaive (n -2)
+
+fib :: Integer -> Integer
+fib n = fib' 0 1 n
+  where
+    fib' a _ 0 = a
+    fib' a b n = fib' b (a+b) (n-1)
+
+      
