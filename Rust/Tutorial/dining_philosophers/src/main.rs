53eeb7366397f9a6060fc64cb090825cb48435c0
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-05-29 12:58:54 +0200

    + finish dining philosophers tutorial

--- Rust/Tutorial/dining_philosophers/src/main.rs
+++ Rust/Tutorial/dining_philosophers/src/main.rs
@@ -7,25 +7,68 @@
 //
 // vim: ft=rust sw=4 ts=4
 
+use std::thread;
+use std::sync::{Mutex, Arc};
+
 // a philosopher struct
 struct Philosopher {
-	name: String,
+    name: String,
+    left: usize,
+    right: usize,
 }
 
 impl Philosopher {
-	fn new(name: &str) -> Philosopher {
-		Philosopher {
-			name: name.to_string(),
-		}
-	}
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+            left: left,
+            right: right,
+        }
+    }
+
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        let _right = table.forks[self.right].lock().unwrap();
+
+        println!("{} is eating.", self.name);
+
+        thread::sleep_ms(1000);
+
+        println!("{} is done eating.", self.name);
+    }
 }
 
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
 
 fn main() {
-	let p1 = Philosopher::new("Baruch Spinoza");
-	let p2 = Philosopher::new("Gilles Deleuze"); 
-	let p3 = Philosopher::new("Karl Marx"); 
-	let p4 = Philosopher::new("Friedich Nietzsche"); 
-	let p5 = Philosopher::new("Michel Foucault"); 
+    let table = Arc::new(Table { forks: vec![
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        Mutex::new(()),
+        ]});
+
+    let philosophers = vec![
+        Philosopher::new("Baruch Spinoza", 0, 1),
+        Philosopher::new("Gilles Deleuze", 1, 2),
+        Philosopher::new("Karl Marx", 2, 3),
+        Philosopher::new("Friedich Nietzsche", 3, 4),
+        Philosopher::new("Michel Foucault", 0, 4), // swap "hands" to prevent deadlock
+        ];
+
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
+
+        thread::spawn(move || {
+            p.eat(&table);
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
 }
 
