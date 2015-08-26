482291d294b45e27cc6b1e0aea810c92a648477a
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-08-26 21:29:21 +0200

    + recursive list functions

--- Elixir/7lang7weeks/day1/print.exs
+++ Elixir/7lang7weeks/day1/print.exs
@@ -10,15 +10,30 @@
 #
 
 defmodule ListExample do
-	
-	def print([]), do: :ok
-	def print([head | tail]) do
-		IO.puts head
-		print tail
-	end
 
-	# alternative implementation
-	def print2(x), do: Enum.each( x, &(IO.puts &1))
+  def print([]), do: :ok
+  def print([head | tail]) do
+    IO.puts head
+    print tail
+  end
 
+  # alternative implementation
+  def print2(x), do: Enum.each( x, &(IO.puts &1))
+
+  # get the length of a list by recursion
+  def my_length([]), do: 0
+  def my_length([_|t]), do: 1 + length(t)
+
+  defp my_max_aux({[], acc}), do: acc
+  defp my_max_aux({[a|tail], acc}) do
+    if a > acc do my_max_aux {tail, a} else my_max_aux {tail, acc} end
+  end
+  def my_max([h|_] = l), do: my_max_aux({l, h})
+
+  defp my_min_aux({[], acc}), do: acc
+  defp my_min_aux({[a|tail], acc}) do
+    if a < acc do my_min_aux {tail, a} else my_min_aux {tail, acc} end
+  end
+  def my_min([h|_] = l), do: my_min_aux({l, h})
 end
 
