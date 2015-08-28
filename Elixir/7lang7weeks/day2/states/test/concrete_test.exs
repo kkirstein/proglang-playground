29ed0573fcb5b5d4fd052a89e1c08633b3a74233
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-08-28 17:23:43 +0200

    + tests for VideoStore state machine

--- Elixir/7lang7weeks/day2/states/test/concrete_test.exs
+++ Elixir/7lang7weeks/day2/states/test/concrete_test.exs
@@ -9,6 +9,20 @@ defmodule ConcreteTest do
     assert rented_video.times_rented == 1
   end
 
+  should "rent video" do
+    rented_video = VideoStore.Concrete.rent video
+    assert :rented == rented_video.state
+    assert 1 == rented_video.times_rented
+  end
+
+  should "handle multiple transitions" do
+    import VideoStore.Concrete
+    vid = video |> rent |> return |> rent |> return |> rent
+    assert :rented == vid.state
+    assert 5 == Enum.count vid.log
+    assert 3 == vid.times_rented
+  end
+
   def video, do: %Video{title: "XMen"}
 
 end
