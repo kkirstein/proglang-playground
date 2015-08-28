a95cec6b8ef733f67dc3afac22df399883092ef0
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-08-28 16:19:20 +0200

    ! use should macro for test definition

--- Elixir/7lang7weeks/day2/states/test/concrete_test.exs
+++ Elixir/7lang7weeks/day2/states/test/concrete_test.exs
@@ -2,8 +2,9 @@
 #
 defmodule ConcreteTest do
   use ExUnit.Case
+  import Should
 
-  test "should update count" do
+  should "update count" do
     rented_video = VideoStore.renting(video)
     assert rented_video.times_rented == 1
   end
--- Elixir/7lang7weeks/day2/states/test/test_helper.exs
+++ Elixir/7lang7weeks/day2/states/test/test_helper.exs
@@ -1,1 +1,13 @@
+# vim: ft=elixir sw=2 ts=2
+#
+
+defmodule Should do
+  defmacro should(name, options) do
+    quote do
+      test("should #{unquote name}", unquote(options))
+    end
+  end
+end
+
 ExUnit.start()
+
