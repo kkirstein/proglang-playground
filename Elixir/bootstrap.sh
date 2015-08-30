5a9a5ab8a504253e6f494a4e5b29c5199e61ac0c
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-08-30 13:54:26 +0200

    + install ntp package

--- Elixir/bootstrap.sh
+++ Elixir/bootstrap.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 # first make sure there is an ntpd config
-if [ ! -e /etc/ntp.conf ]; then
-	cat > /etc/ntp.conf << EOS
-driftfile /var/lib/ntp/ntp.drift
-server 0.pool.ntp.org
-server 1.pool.ntp.org
-server 2.pool.ntp.org
-server pool.ntp.org
-EOS
-fi
+#if [ ! -e /etc/ntp.conf ]; then
+#	cat > /etc/ntp.conf << EOS
+#driftfile /var/lib/ntp/ntp.drift
+#server 0.pool.ntp.org
+#server 1.pool.ntp.org
+#server 2.pool.ntp.org
+#server pool.ntp.org
+#EOS
+#fi
+
+# install and/or reload ntp daemon
+apt-get -y install ntp
+service ntp reload
 
 #wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && dpkg -i erlang-solutions_1.0_all.deb 
 curl --insecure --silent https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb -O
