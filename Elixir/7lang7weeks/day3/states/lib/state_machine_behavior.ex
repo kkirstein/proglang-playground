36bfcc69829ac158cac5ecfcf74e9244d40cc5f7
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-08-30 13:20:51 +0200

    ! beautify docs

--- Elixir/7lang7weeks/day3/states/lib/state_machine_behavior.ex
+++ Elixir/7lang7weeks/day3/states/lib/state_machine_behavior.ex
@@ -7,7 +7,7 @@
 defmodule StateMachine.Behavior do
   @doc """
   Updates the context to the new state and activates
-  the associated functions from event.calls
+  the associated functions from event.calls.
   """
   def fire(context, event) do
     %{context | state: event[:to]}
@@ -16,7 +16,7 @@ defmodule StateMachine.Behavior do
 
   @doc """
   Alternative api to fire an event, with lookup of the
-  next state by event_name and the provided states description
+  next state by event_name and the provided states description.
   """
   def fire(states, context, event_name) do
     event = states[context.state][event_name]
@@ -24,7 +24,7 @@ defmodule StateMachine.Behavior do
   end
 
   @doc """
-  Call the associated functions with updated context respectively
+  Call the associated functions with updated context respectively.
   """
   def activate(context, event) do
     Enum.reduce(event[:calls] || [], context, &(&1.(&2)))
