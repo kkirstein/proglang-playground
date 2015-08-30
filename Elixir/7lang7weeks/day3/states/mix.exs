9a39c148d3244acb2b78e3a80f480477698dc425
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-08-30 13:37:15 +0200

    + setup supervisor & application startup

--- Elixir/7lang7weeks/day3/states/mix.exs
+++ Elixir/7lang7weeks/day3/states/mix.exs
@@ -14,8 +14,7 @@ defmodule States.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger],
-     mod: {States, []}]
+    [ mod: {States, []}]
   end
 
   # Dependencies can be Hex packages:
