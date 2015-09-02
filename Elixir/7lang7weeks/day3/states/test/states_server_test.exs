e8cefb453219ee5452b0ad3b82553ed530266e81
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-09-02 17:02:29 +0200

    + test case for GenServer setup (fixed)

--- Elixir/7lang7weeks/day3/states/test/states_server_test.exs
+++ Elixir/7lang7weeks/day3/states/test/states_server_test.exs
@@ -9,9 +9,23 @@ defmodule StatesServerTest do
 
   test "rent a video" do
     assert GenServer.cast(:video_store, {:add, {:murmel, %Video{title: "Murmeltier"}}}) == :ok
-    {id, %{title: t, times_rented: r, state: s}} = GenServer.call(:video_store, {:rent, :murmel})
+    %{title: t, times_rented: r, state: s} = GenServer.call(:video_store, {:rent, :murmel})
+    assert t == "Murmeltier"
+    assert r == 1
+    assert s == :rented
   end
 
-
+  test "mutiple transitions" do
+    assert GenServer.cast(:video_store, {:add, {:murmel, %Video{title: "Murmeltier"}}}) == :ok
+    GenServer.call(:video_store, {:rent, :murmel})
+    GenServer.call(:video_store, {:return, :murmel})
+    GenServer.call(:video_store, {:rent, :murmel})
+    GenServer.call(:video_store, {:return, :murmel})
+    %{title: t, times_rented: r, state: s, log: l} = GenServer.call(:video_store, {:rent, :murmel})
+    assert "Murmeltier" == t
+    assert 3            == r
+    assert :rented      == s
+    assert 5            == Enum.count l
+  end
 end
 
