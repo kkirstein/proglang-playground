ee8e81deef82e0d060bd108ae2beedf4859eecbb
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-09-10 17:49:00 +0200

    + use pipe macros

--- Elixir/benchmark/lib/Benchmark/perfect_number.ex
+++ Elixir/benchmark/lib/Benchmark/perfect_number.ex
@@ -5,6 +5,7 @@
 #
 
 defmodule Benchmark.PerfectNumber do
+  use Pipe
 
   @doc """
   Predicate checks whether a given number is perfect
@@ -29,10 +30,22 @@ defmodule Benchmark.PerfectNumber do
     for x <- 1..limit, is_perfect?(x), do: x
   end
 
+  @doc """
+  Returns a list of perfect numbers for given upper limit.
+  Calculations are performed asynchronous with Task.async
+  """
   def perfect_numbers_async(limit) do
-    tt = Enum.map(1..limit, fn(x) -> Task.async(fn -> {is_perfect?(x), x} end) end)
-    Keyword.get_values Enum.map(tt, fn(x) -> Task.await(x) end), true
+    kl = pipe_with fn(ll, f) -> Enum.map(ll, f) end,
+      Enum.to_list(1..limit) |> spawn_task |> Task.await
+    filter_true(kl)
+
+    #tt = Enum.map(1..limit, fn(x) -> Task.async(fn -> {is_perfect?(x), x} end) end)
+    #Keyword.get_values Enum.map(tt, fn(x) -> Task.await(x) end), true
   end
+
+  defp spawn_task(x), do: Task.async(fn -> {is_perfect?(x), x} end)
+  defp filter_true(kl), do: Keyword.get_values(kl, true)
+
 
 end
 
--- Elixir/benchmark/mix.exs
+++ Elixir/benchmark/mix.exs
@@ -27,6 +27,6 @@ defmodule Benchmark.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [{:pipe, "~> 0.0.2"}]
   end
 end
