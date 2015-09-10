b10ed4cc27d0d94045a7e050c9506fd313c4001c
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-09-10 10:51:42 +0200

    + set timezone of vagrant box

--- Elixir/bootstrap.sh
+++ Elixir/bootstrap.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
-# first make sure there is an ntpd config
-#if [ ! -e /etc/ntp.conf ]; then
-#	cat > /etc/ntp.conf << EOS
-#driftfile /var/lib/ntp/ntp.drift
-#server 0.pool.ntp.org
-#server 1.pool.ntp.org
-#server 2.pool.ntp.org
-#server pool.ntp.org
-#EOS
-#fi
 
 # install and/or reload ntp daemon
 apt-get -y install ntp
 service ntp reload
+
+# setup correct timezone
+echo "Europe/Zurich" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
 
 #wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && dpkg -i erlang-solutions_1.0_all.deb 
 curl --insecure --silent https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb -O
