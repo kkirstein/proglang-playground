68e8588d76a8f950a7fae07f5429d7459df4f1b9
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-11-02 14:58:25 +0100

    ! fix perfect numbers test

--- Go/work/src/benchmark/perfectnumber/perfectnumber_test.go
+++ Go/work/src/benchmark/perfectnumber/perfectnumber_test.go
@@ -1,24 +1,28 @@
 // perfectnumber_test.go
-// 
+//
 // vim: ft=go sw=4 ts=4
 //
 
 package perfectnumber
 
-import (
-	"testing"
-	"reflect"
-)
+import "testing"
 
 func TestIsPerfect(t *testing.T) {
 	switch {
-	case IsPerfect(1): t.Error("1 is no perfect number")
-	case IsPerfect(2): t.Error("2 is no perfect number")
-	case IsPerfect(3): t.Error("3 is no perfect number")
-	case IsPerfect(4): t.Error("4 is no perfect number")
-	case IsPerfect(5): t.Error("5 is no perfect number")
-	case ! IsPerfect(6): t.Error("6 is perfect number")
-	case IsPerfect(7): t.Error("7 is no perfect number")
+	case IsPerfect(1):
+		t.Error("1 is no perfect number")
+	case IsPerfect(2):
+		t.Error("2 is no perfect number")
+	case IsPerfect(3):
+		t.Error("3 is no perfect number")
+	case IsPerfect(4):
+		t.Error("4 is no perfect number")
+	case IsPerfect(5):
+		t.Error("5 is no perfect number")
+	case !IsPerfect(6):
+		t.Error("6 is perfect number")
+	case IsPerfect(7):
+		t.Error("7 is no perfect number")
 	}
 }
 
@@ -28,9 +32,9 @@ func TestPerfectNumbers(t *testing.T) {
 	if len(pn) != len(exp) {
 		t.Error("Wrong number of perfect numbers")
 	}
-	if reflect.DeepEqual(pn, exp) {
-		t.Errorf("Perfect numbers below 1000: expected: %v, got: %v", exp, pn)
+	for i, v := range pn {
+		if v != exp[i] {
+			t.Errorf("Wrong perfect number, expected %d, actual: %d", exp[i], v)
+		}
 	}
 }
-
-
