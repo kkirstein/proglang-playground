656d8a7233b851df6e533a3e09b8be73c6de6e5e
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2016-02-05 17:08:51 +0100

    + generate image struct

--- OCaml/Benchmark/mandelbrot.ml
+++ OCaml/Benchmark/mandelbrot.ml
@@ -7,6 +7,10 @@
 (* struct for pixel color *)
 type pixel_color = { r : int; g : int; b : int }
 
+(* image data container *)
+type image = { width : int; height : int; data : pixel_color array }
+
+
 (* calculate pixel value *)
 let pixel_value x y n_max r_max =
   let z0 = {Complex.re = x; Complex.im = y} in
@@ -17,7 +21,6 @@ let pixel_value x y n_max r_max =
   loop Complex.zero n_max
 ;;
 
-
 (* select color for given pixel value *)
 let color_black = { r = 0; g = 0; b = 0 }
 let color_white = { r = 255; g = 255; b = 255 }
@@ -25,9 +28,17 @@ let color_of_value value =
   if value > 0 then color_white else color_black
 ;;
 
-
 (* generate image data *)
-let image_data width height = 
-  Array.make_matrix width height color_black
+let image ?color:(color=color_black) width height = 
+  { width=width; height=height; data=Array.make (width*height) color }
+;;
+
+let pixel_iter f img =
+  let rec loop x y =
+    if x < (img.width-1) then (ignore(f x y img); loop (x+1) y)
+    else (if y < (img.height-1) then (ignore(f x y img); loop 0 (y+1))
+      else ())
+  in
+  loop 0 0
 ;;
 
