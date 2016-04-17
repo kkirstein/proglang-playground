4494708b81af884f644399f26c1bb062e1ca4a24
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2016-04-17 18:02:16 +0200

    + skeleton for Fortran benchmark

---
+++ Fortran/Benchmark/benchmark.f95
@@ -0,0 +1,33 @@
+! benchmark.f
+! A selectoin of micro-benchmarks for Fortran
+!
+! vim: set ft=fortran sw=8 ts=8 :
+
+      program benchmark
+
+      implicit none
+
+
+      write (*,*) "Fibonacci numbers"
+      write (*,*) "================="
+      write (*,*) "todo.."
+
+      write (*,*) ""
+
+      write (*,*) "Perfect numbers"
+      write (*,*) "==============="
+      write (*,*) "todo.."
+
+      write (*,*) ""
+
+      write (*,*) "Mandelbrot set"
+      write (*,*) "=============="
+      write (*,*) "todo.."
+
+      write (*,*) ""
+
+      write (*,*) " done!"
+
+
+      end program benchmark
+
---
+++ Fortran/Benchmark/fibonacci.f95
@@ -0,0 +1,14 @@
+! benchmark.f
+! A selectoin of micro-benchmarks for Fortran
+!
+! vim: set ft=fortran sw=8 ts=8 :
+
+      integer function fib_naive ( n )
+
+      implicit none
+
+      integer :: n
+
+
+      end function fib_naive
+
