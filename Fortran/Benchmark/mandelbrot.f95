b98fa82731ba44c2a2fcbe209437398b77d51a13
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2016-04-20 11:25:43 +0200

    + pixel_value of mandelbrot set

--- Fortran/Benchmark/mandelbrot.f95
+++ Fortran/Benchmark/mandelbrot.f95
@@ -12,11 +12,32 @@ module mandelbrot
 contains
   ! TODO: function defs
 
-  real function pixel_value (z)
+  integer function pixel_value (z, n_max)
+    implicit none
 
     complex, intent( in ) :: z
+    integer, optional, intent( in ) :: n_max
 
-    pixel_value = 2.0
+    complex :: z1
+    integer :: n, n_end
+
+    if ( present(n_max) ) then
+      n_end = n_max
+    else
+      n_end = 256
+    end if
+    z1 = 0
+
+    do n = 0, n_end
+      if (abs(z) > r_max) then
+        pixel_value = n
+        return
+      end if
+      z1 = z1**2 + z
+    end do
+
+    pixel_value = 0
+
   end function pixel_value
 
 
