457a60baa964af0679e3203de2c54a90a156c4a8
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2016-04-27 19:56:54 +0200

    + start lwt tutorial

---
+++ OCaml/Playground/lwt_tutorial.ml
@@ -0,0 +1,14 @@
+#use "topfind";;
+#require "lwt.simple-top";;
+
+open Lwt
+;;
+
+Lwt.state (Lwt.return 42);;
+Lwt.state (Lwt.fail Exit);;
+
+let waiter, wakener = Lwt.wait ();;
+Lwt.state waiter;;
+Lwt.wakeup wakener 42;;
+Lwt.state waiter;;
+
