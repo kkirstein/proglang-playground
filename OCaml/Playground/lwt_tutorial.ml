60ee55f989967ed406e46744cbbb28093c399310
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2016-05-17 17:39:40 +0200

    + Lwt tutorial

--- OCaml/Playground/lwt_tutorial.ml
+++ OCaml/Playground/lwt_tutorial.ml
@@ -3,6 +3,7 @@
 
 open Lwt;;
 
+(* thread creation *)
 Lwt.state (Lwt.return 42);;
 Lwt.state (Lwt.fail Exit);;
 
@@ -10,4 +11,14 @@ let waiter, wakener = Lwt.wait ();;
 Lwt.state waiter;;
 Lwt.wakeup wakener 42;;
 Lwt.state waiter;;
+
+let waiter, wakener = Lwt.wait ();;
+Lwt.state waiter;;
+Lwt.wakeup_exn wakener Exit;;
+Lwt.state waiter;;
+
+(* thread composition *)
+Lwt.bind
+  (Lwt_io.read_line Lwt_io.stdin)
+  (fun str -> Lwt_io.printlf "You typed %S" str);;
 
