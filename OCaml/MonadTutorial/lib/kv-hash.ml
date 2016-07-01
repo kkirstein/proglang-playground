86062d95356f4888e4ae9d2fdb9a01df3517005d
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2016-07-01 22:03:58 +0200

    + setup MonadTutorial

---
+++ OCaml/MonadTutorial/lib/kv-hash.ml
@@ -0,0 +1,6 @@
+(** A Ocaml tutorial to show the usage of Monads
+ * It implements a DSL for a key-value store, using
+ * different backend implementation, e.g. Hashtbl or Assoc Lists
+ *
+ * see: http://www.heise.de/developer/artikel/Dependency-Injection-in-der-funktionalen-Programmierung-3115570.html
+ *)
