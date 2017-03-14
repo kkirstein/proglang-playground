b96e607c25a4688f3337073ee9f7f96053f3b522
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2017-03-14 13:35:09 +0100

    ! update swift version

--- Swift/bootstrap.sh
+++ Swift/bootstrap.sh
@@ -13,27 +13,30 @@ dpkg-reconfigure -f noninteractive tzdata
 # install swift
 echo Downloading Swift
 echo =================
+
+# https://swift.org/builds/swift-3.0-preview-6/ubuntu1510/swift-3.0-PREVIEW-6/swift-3.0-PREVIEW-6-ubuntu15.10.tar.gz
+#
 branch=development
-version=DEVELOPMENT-SNAPSHOT-2016-05-31-a
-#version=2.2.1-SNAPSHOT-2016-03-28-a
-platform=ubuntu14.04
+version=3.0-preview-6
+platform=ubuntu15.04
 if [ ! -f swift-${version}-${platform}.tar.gz ]; then
-	curl --silent --insecure https://swift.org/builds/${branch}/ubuntu1404/swift-${version}/swift-${version}-${platform}.tar.gz -O
+	curl --silent --insecure https://swift.org/builds/swift-${version}/ubuntu1510/swift-${version}/swift-${version}-${platform}.tar.gz -O
 fi
-curl --silent --insecure https://swift.org/builds/${branch}/ubuntu1404/swift-${version}/swift-${version}-${platform}.tar.gz.sig -O
+curl --silent --insecure https://swift.org/builds/swift-${version}/ubuntu1510/swift-${version}/swift-${version}-${platform}.tar.gz.sig -O
 
 echo Verifying signatures
 echo ====================
 gpg --keyserver hkp://pool.sks-keyservers.net \
 	--recv-keys \
 	'7463 A81A 4B2E EA1B 551F  FBCF D441 C977 412B 37AD' \
-	'1BE1 E29A 084C B305 F397  D62A 9F59 7F4D 21A5 6D5F'
+	'1BE1 E29A 084C B305 F397  D62A 9F59 7F4D 21A5 6D5F' \
+	'A3BA FD35 56A5 9079 C068  94BD 63BC 1CFE 91D3 06C6'
 gpg --keyserver hkp://pool.sks-keyservers.net --refresh-keys Swift
 gpg --verify swift-${version}-${platform}.tar.gz.sig
 
 echo Installing Swift
 echo =============== 
-apt-get -y install clang
+apt-get -y install clang libicu-dev
 tar -C /usr/local -xzf swift-${version}-${platform}.tar.gz
 
 if [ ! `which swift` ]; then
