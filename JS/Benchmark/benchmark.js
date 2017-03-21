f0e9f06b56e4cce8a7ebb42c503032c7ec459eff
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2017-03-21 22:36:13 +0100

    ! calculate FIbonacci numbers async

--- JS/Benchmark/benchmark.js
+++ JS/Benchmark/benchmark.js
@@ -6,85 +6,93 @@
 
 'use strict';
 
-const fib = require("./fib");
-const BigInt = require('BigInt');
-const pn = require("./perfect_numbers");
-const mandel = require("./mandelbrot");
-
-console.log("JavaScript benchmarks");
-console.log("=====================");
-console.log();
-
-console.log("Fibonacci numbers:");
-console.log("------------------");
-
-var tic = new Date();
-var res = fib.fib_naive(35);
-var toc = new Date();
-console.log("fib_naive(35) = ", res, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
-
-var tic = new Date();
-var fib_promise = fib.fib_naive_async(35);
-fib_promise.then((res) => {
-  var toc = new Date();
-  console.log("fib_naive_async(35) = ", res, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
-});
-
-tic = new Date();
-res = fib.fib(35);
-toc = new Date();
-console.log("fib(35) = ", BigInt.bigInt2str(res, 10), "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
-
-tic = new Date();
-res = fib.fib(1000);
-toc = new Date();
-console.log("fib(1000) = ", BigInt.bigInt2str(res, 10), "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
-console.log();
-
-console.log("Perfect numbers:");
-console.log("----------------");
-
-tic = new Date();
-res = pn.perfect_numbers(10000);
-toc = new Date();
-console.log("perfect_numbers(10000) = ", res, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
-tic = new Date();
-res = [];
-// the following version seems not to be completely lazy:
-//for (let n of pn.sequence()) {
-//   	if (n < 1000) res.push(n); else break;
-//}
-var gen = pn.sequence();
-for (let i=0; i<4; i++) {
-   	res.push(gen.next().value);
+// time_it helper
+async function time_it(fun, ...args) {
+  return new Promise((resolve, reject) => {
+    const tic = new Date();
+    const res = fun(...args);
+    const toc = new Date();
+    resolve({"result": res, "elapsed": toc.getTime()-tic.getTime()});
+  });
 }
-toc = new Date();
-console.log("4 perfect numbers (Generator) = ", res, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
-console.log();
+async function time_it_async(fun, ...args) {
+  const tic = new Date();
+  const res = await fun(...args);
+  const toc = new Date();
+  return {"result": res, "elapsed": toc.getTime()-tic.getTime()};
+}
+
+// async entry point
+(async () => {
+  const fib = require("./fib");
+  const BigInt = require('BigInt');
+  const pn = require("./perfect_numbers");
+  const mandel = require("./mandelbrot");
+
+  console.log("JavaScript benchmarks");
+  console.log("=====================");
+  console.log();
+
+  console.log("Fibonacci numbers:");
+  console.log("------------------");
+
+  const fib_naive = await time_it_async(fib.fib_naive, 35);
+  console.log("fib_naive(35) = ", fib_naive.result, "\tElapsed: ", fib_naive.elapsed, "ms.");
+
+  const fib_35 = await time_it_async(fib.fib, 35);
+  console.log("fib(35) = ", BigInt.bigInt2str(fib_35.result, 10),
+  "\tElapsed: ", fib_35.elapsed, "ms.");
+
+  const fib_1000 = await time_it_async(fib.fib, 1000);
+  console.log("fib(1000) = ", BigInt.bigInt2str(fib_1000.result, 10),
+  "\tElapsed: ", fib_1000.elapsed, "ms.");
+
+  console.log();
+  console.log("Perfect numbers:");
+  console.log("----------------");
+
+  let tic = new Date();
+  let res = pn.perfect_numbers(10000);
+  let toc = new Date();
+  console.log("perfect_numbers(10000) = ", res, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
+  tic = new Date();
+  res = [];
+  // the following version seems not to be completely lazy:
+  //for (let n of pn.sequence()) {
+  //   	if (n < 1000) res.push(n); else break;
+  //}
+  var gen = pn.sequence();
+  for (let i=0; i<4; i++) {
+    res.push(gen.next().value);
+  }
+  toc = new Date();
+  console.log("4 perfect numbers (Generator) = ", res, "\tElapsed: ", (toc.getTime()-tic.getTime()), "ms.");
+  console.log();
 
 
-console.log("Mandelbrot set:");
-console.log("--------------");
+  console.log("Mandelbrot set:");
+  console.log("--------------");
 
-var tic11 = new Date();
-let image = mandel.mandelbrot(640, 480, -0.5, 0.0, 4.0/640);
-var toc11 = new Date();
-console.log("Mandelbrot set (640x480) calculated!", "\tElapsed: ", (toc11.getTime()-tic11.getTime()), "ms.");
+  var tic11 = new Date();
+  let image = mandel.mandelbrot(640, 480, -0.5, 0.0, 4.0/640);
+  var toc11 = new Date();
+  console.log("Mandelbrot set (640x480) calculated!", "\tElapsed: ", (toc11.getTime()-tic11.getTime()), "ms.");
 
-var tic12 = new Date();
-var promise = mandel.mandelbrot_async(640, 480, -0.5, 0.0, 4.0/640);
-promise.then(function (res) {
-	var toc12 = new Date();
-	console.log("Mandelbrot set (640x480) calculated (async)!", "\tElapsed: ", (toc12.getTime()-tic12.getTime()), "ms.");
-});
+  var tic12 = new Date();
+  var promise = mandel.mandelbrot_async(640, 480, -0.5, 0.0, 4.0/640);
+  promise.then(function (res) {
+    var toc12 = new Date();
+    console.log("Mandelbrot set (640x480) calculated (async)!", "\tElapsed: ", (toc12.getTime()-tic12.getTime()), "ms.");
+  });
 
-var tic13 = new Date();
-mandel.writePGM("./mandelbrot.pgm", image);
-var toc13 = new Date();
-console.log("Mandelbrot set (640x480) written!", "\tElapsed: ", (toc13.getTime()-tic13.getTime()), "ms.");
-console.log();
+  var tic13 = new Date();
+  mandel.writePGM("./mandelbrot.pgm", image);
+  var toc13 = new Date();
+  console.log("Mandelbrot set (640x480) written!", "\tElapsed: ", (toc13.getTime()-tic13.getTime()), "ms.");
+  console.log();
 
-// wait for all pending promises
-// TODO
-console.log("------------------");
-console.log("Done!");
+  // wait for all pending promises
+  // TODO
+  console.log("------------------");
+  console.log("Done!");
+})()
--- JS/Benchmark/fib.js
+++ JS/Benchmark/fib.js
@@ -7,24 +7,26 @@
 var BigInt = require('BigInt');
 
 // naive & efficient (BigInt) implementation
-function fib_naive (n) {
+function _fib_naive (n) {
 	return (n < 2)? n : arguments.callee(n-1) + arguments.callee(n-2);
 }
 
-function fib_naive_async (n) {
+function fib_naive (n) {
 	return new Promise(function (resolve, reject) {
-		let result = fib_naive(n);
+		let result = _fib_naive(n);
 		resolve(result);
 	})
 }
 
 function fib (n) {
-	return function(n, a, b) {
-		return (n === 0)? a : arguments.callee(n-1, b, BigInt.add(a, b));
-	}(n, new BigInt.int2bigInt(0, 32), new BigInt.int2bigInt(1, 32));
+	return new Promise((resolve, reject) => {
+		const res = function(n, a, b) {
+			return (n === 0)? a : arguments.callee(n-1, b, BigInt.add(a, b));
+		}(n, new BigInt.int2bigInt(0, 32), new BigInt.int2bigInt(1, 32));
+		resolve(res);
+	});
 }
 
 // exports
 exports.fib_naive = fib_naive;
-exports.fib_naive_async = fib_naive_async;
 exports.fib = fib;
