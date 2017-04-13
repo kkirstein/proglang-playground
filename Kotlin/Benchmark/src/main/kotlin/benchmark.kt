29368df9e0f0cc2dc7a1612082cb01a1a457baf1
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2017-04-13 21:10:35 +0200

    + skeleton for fib package

--- Kotlin/Benchmark/src/main/kotlin/benchmark.kt
+++ Kotlin/Benchmark/src/main/kotlin/benchmark.kt
@@ -1,6 +1,6 @@
 /* benchmark.kt
  * A set of (micro-) benchmarks for the Kotlin
- * (http:kotlinlang.org) programming language
+ * (http://kotlinlang.org) programming language
  */
 
 package benchmark
---
+++ Kotlin/Benchmark/src/main/kotlin/benchmark/fib.kt
@@ -0,0 +1,6 @@
+/* fib.kt
+ * Calculate Fibonacci numbers in the Kotlin
+ * (http://kotlinlang.org) programming language
+ */
+
+package fib
