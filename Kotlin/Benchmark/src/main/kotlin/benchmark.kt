a5121cc774d0727362144e17752cb559d000fcf8
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2017-04-21 20:04:36 +0200

    + tail-call optimized version of Fibonacci

--- Kotlin/Benchmark/src/main/kotlin/benchmark.kt
+++ Kotlin/Benchmark/src/main/kotlin/benchmark.kt
@@ -18,8 +18,15 @@ fun main(args: Array<String>) {
   println("Fibonacci numbers:");
   println("------------------");
   val res = fib_naive(35)
-  val elap = measureTimeMillis { fib_naive(35) }
+  var elap = measureTimeMillis { fib_naive(35) }
   println("fib_naive(35) = $res, elapsed time: $elap ms.")
+  var res2 = fib(35)
+  elap = measureTimeMillis { fib(35) }
+  println("fib(35) = $res2, elapsed time: $elap ms.")
+  res2 = fib(1000)
+  elap = measureTimeMillis { fib(1000) }
+  println("fib(1000) = $res2, elapsed time: $elap ms.")
+
   println();
 
   println("------------------");
--- Kotlin/Benchmark/src/main/kotlin/benchmark/fibonacci.kt
+++ Kotlin/Benchmark/src/main/kotlin/benchmark/fibonacci.kt
@@ -5,11 +5,23 @@
 
 package fibonacci
 
+import java.math.BigInteger
+
 // naive implementation
-fun fib_naive(n: Int) : Int {
+fun fib_naive(n: Int): Int {
   if (n < 2) {
     return n
   } else {
     return (fib_naive(n-1) + fib_naive(n-2))
   }
+}
+
+// optimized tail-recursive implementation
+
+fun fib(n: Int): BigInteger {
+  fun loop(a: BigInteger, b: BigInteger, n: Int): BigInteger {
+    return if (n == 0) a else loop(b, a + b, n - 1)
+  }
+
+  return loop(BigInteger.valueOf(0), BigInteger.valueOf(1), n)
 }
