65460d952d07999c67e2ca6c488e8140212ed859
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2017-05-08 21:10:33 +0200

    + setup IDE

--- Kotlin/Benchmark/src/main/kotlin/benchmark.kt
+++ Kotlin/Benchmark/src/main/kotlin/benchmark.kt
@@ -13,42 +13,42 @@ import fibonacci.*
 import perfectnumber.*
 
 // main entry point
-fun main(args: Array<String>) {
+fun main(args: Array<String>) = runBlocking {
 
-  println("Kotlin benchmarks");
-  println("=================");
-  println();
+    println("Kotlin benchmarks")
+    println("=================")
+    println()
 
-  println("Fibonacci numbers:");
-  println("------------------");
+    //val jobs = List()
 
-  launch(CommonPool) {
-    /*delay(100L)
-    println("Checkpoint!")*/
-    val res = timeIt2 { fibNaiveAsync(35) }
-    println("fibNaive(35) = ${res.result} (Async), elapsed time: ${res.elapsed} ms.")
-  }
+    println("Fibonacci numbers:")
+    println("------------------")
 
-  val res1 = timeit { fibNaive(35) }
-  println("fibNaive(35) = ${res1.result}, elapsed time: ${res1.elapsed} ms.")
-  val res2 = timeit { fib(35) }
-  println("fib(35) = ${res2.result}, elapsed time: ${res2.elapsed} ms.")
-  val res3 = timeit { fib(1000) }
-  println("fib(1000) = ${res3.result}, elapsed time: ${res3.elapsed} ms.")
+    val job = launch(CommonPool) {
+        val res = timeIt2 { fibNaiveAsync(35) }
+        println("fibNaive(35) = ${res.result} (Async), elapsed time: ${res.elapsed} ms.")
+    }
 
-  println();
+    val res1 = timeit { fibNaive(35) }
+    println("fibNaive(35) = ${res1.result}, elapsed time: ${res1.elapsed} ms.")
+    val res2 = timeit { fib(35) }
+    println("fib(35) = ${res2.result}, elapsed time: ${res2.elapsed} ms.")
+    val res3 = timeit { fib(1000) }
+    println("fib(1000) = ${res3.result}, elapsed time: ${res3.elapsed} ms.")
 
-  println("Perfect numbers:");
-  println("----------------");
-  val resPn = timeit { perfectNumbers(10000) }
-  println("perfectNumbers(10000) = ${resPn.result}, elapsed time: ${resPn.elapsed} ms.")
-  val resPnSeq = timeit { perfectNumberSeq.take(5).toList() }
-  println("perfectNumberSeq(5) = ${resPnSeq.result}, elapsed time: ${resPnSeq.elapsed} ms.")
+    println()
 
-  println();
+    println("Perfect numbers:")
+    println("----------------")
+    val resPn = timeit { perfectNumbers(10000) }
+    println("perfectNumbers(10000) = ${resPn.result}, elapsed time: ${resPn.elapsed} ms.")
+    val resPnSeq = timeit { perfectNumberSeq.take(5).toList() }
+    println("perfectNumberSeq(5) = ${resPnSeq.result}, elapsed time: ${resPnSeq.elapsed} ms.")
 
-  println("------------------");
-  Thread.sleep(1000L)
-  println("Done.");
+    println()
+
+    println("------------------")
+    job.join()
+    println("Done.")
 
 }
