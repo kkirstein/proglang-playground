be6ecfad8759afc66cdcdf5359964ff20198b70d
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2017-09-04 19:33:22 +0200

    + larger Mandelbrot set

--- Crystal/my-benchmark/src/my-benchmark.cr
+++ Crystal/my-benchmark/src/my-benchmark.cr
@@ -44,7 +44,8 @@ module MyBenchmark
     end
 
     Benchmark.ips do |x|
-			x.report("Mandelbrot::Image.new") { Mandelbrot::Image.new(640, 480, -0.5f32, 0.0f32, 4.0f32 / 640) }
+			x.report("Mandelbrot (640x480)") { Mandelbrot::Image.new(640, 480, -0.5f32, 0.0f32, 4.0f32 / 640) }
+			x.report("Mandelbrot (1920x1080)") { Mandelbrot::Image.new(1920, 1080, -0.5f32, 0.0f32, 4.0f32 / 1920) }
     end
   end
 end
