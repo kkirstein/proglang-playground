370a06158e4d564afaf9fc61ec390af72356a0a7
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2018-09-21 10:46:00 +0200

    + start Mandelbrot calculations

---
+++ Rust/Benchmark/src/mandelbrot.rs
@@ -0,0 +1,19 @@
+// mandelbrot.rs
+// Calulate Mandelbrot sets in Rust
+//
+// vim: ft=rust sw=4 ts=4
+//
+
+use std::num::Complex;
+
+pub fn pixel_value(x: f64, y: f64) -> u8 {
+
+    let z0 = new Complex(x, y);
+    let mut z = new Complex(0, 0);
+
+    while () {
+        // TODO
+    }
+    0
+}
+
