622a8872f75321ac32ce42d19a7b07a054605dc1
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2018-09-25 15:08:34 +0200

    ! fix function names for F# standard

--- FSharp/Benchmark/Tasks/Perfectnumber.fs
+++ FSharp/Benchmark/Tasks/Perfectnumber.fs
@@ -5,7 +5,7 @@ namespace Tasks
 module Perfectnumber =
 
     // predicate to check whether a number is perfect
-    let is_perfect n =
+    let isPerfect n =
         let rec loop i sum =
             if n = i then sum = n else
             if n % i = 0 then loop (i + 1) (sum + i) else loop (i + 1) sum
@@ -13,10 +13,10 @@ module Perfectnumber =
         if n > 0 then loop 1 0 else failwith "n must > 0"
 
     // generate a list of perfect numbers until given limit
-    let perfect_numbers n =
+    let perfectNumbers n =
         let rec loop i =
             if i = n then []
             else
-                if is_perfect i then i :: loop (i + 1) else loop (i + 1)
+                if isPerfect i then i :: loop (i + 1) else loop (i + 1)
         in
         loop 1
