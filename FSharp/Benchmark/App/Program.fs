f399023043c6155209149b3617337a0f07e33967
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2019-03-25 13:08:41 +0100

    fix idiomatic function names

--- FSharp/Benchmark/App/Program.fs
+++ FSharp/Benchmark/App/Program.fs
@@ -6,8 +6,8 @@ open Tasks.Perfectnumber
 open Tasks.Mandelbrot
 
 // measures the elapsed tim eof the given function
-let time_it f =
-    let timer = new Stopwatch () in
+let timeIt f =
+    let timer = Stopwatch () in
     timer.Start ()
     let res = f ()
     timer.Stop ()
@@ -20,25 +20,25 @@ let main argv =
     printfn ""
     printfn "Fibonacci numbers"
     printfn "-----------------"
-    let res, elap = time_it (fun () -> fibNaive 35) in
+    let res, elap = timeIt (fun () -> fibNaive 35) in
     printfn "fibNaive(35): %d (%d ms)" res elap
-    let res, elap = time_it (fun () -> fib 35) in
+    let res, elap = timeIt (fun () -> fib 35) in
     printfn "fib(35): %A (%d ms)" res elap
-    let res, elap = time_it (fun () -> fib 1000) in
+    let res, elap = timeIt (fun () -> fib 1000) in
     printfn "fib(1000): %A (%d ms)" res elap
     printfn ""
     printfn "Perfect numbers"
     printfn "---------------"
-    let res, elap = time_it (fun () -> perfectNumbers 10000) in
+    let res, elap = timeIt (fun () -> perfectNumbers 10000) in
     printfn "perfectNumbers(10000): %A (%d ms)" res elap
-    let res, elap = time_it (fun () -> perfectNumbersPar 10000) in
+    let res, elap = timeIt (fun () -> perfectNumbersPar 10000) in
     printfn "perfectNumbersPar(10000): %A (%d ms)" res elap
     printfn ""
     printfn "Mandelbrot set"
     printfn "--------------"
-    let res, elap = time_it (fun () -> mandelbrot 1920 1200 -0.5 0.0 (4.0/1920.0)) in
+    let res, elap = timeIt (fun () -> mandelbrot 1920 1200 -0.5 0.0 (4.0/1920.0)) in
     printfn "mandelbrot(1920 x 1200): done (%d ms)" elap
-    let res, elap = time_it (fun () -> writePNM res @"./mandelbrot.pnm") in
+    let res, elap = timeIt (fun () -> writePNM res @"./mandelbrot.pnm") in
     printfn "mandelbrot(1920 x 1200): written (%d ms)" elap
     printfn "-----"
     printfn "Done!"
