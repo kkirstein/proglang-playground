10a26064e5fd04c6747885b2748653a92a018151
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2019-04-01 14:54:15 +0200

    fix format

--- Rust/Tutorial/guessing_game/src/main.rs
+++ Rust/Tutorial/guessing_game/src/main.rs
@@ -8,9 +8,9 @@
 
 extern crate rand;
 
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
 	println!("Guess the number!");
@@ -24,25 +24,27 @@ fn main() {
 
 		let mut guess = String::new();
 
-		io::stdin().read_line(&mut guess)
+		io::stdin()
+			.read_line(&mut guess)
 			.expect("Failed to read line");
 
 		// convert to number
 		let guess: u32 = match guess.trim().parse() {
-			Ok(num)	=> num,
-			Err(_)	=> continue
+			Ok(num) => num,
+			Err(_) => continue,
 		};
 
 		println!("You guessed: {}", guess);
 
 		match guess.cmp(&secret_number) {
-			Ordering::Less		=> println!("Too small!"),
-			Ordering::Greater	=> println!("Too big!"),
-			Ordering::Equal		=> {
+			Ordering::Less => println!("Too small!"),
+			Ordering::Greater => println!("Too big!"),
+			Ordering::Equal => {
 				println!("You win!");
 				break;
 			}
 		}
 	}
-}
-
+}thread::spawn(move || {
+	
+});
