5610e859fab598932fd2890d1d62d18491cd6c3d
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2019-06-04 15:28:22 +0200

    generate plots for each time step

--- Fortran/ModernFortran/ocaml/plot_1d_time_steps.ml
+++ Fortran/ModernFortran/ocaml/plot_1d_time_steps.ml
@@ -1,23 +1,34 @@
 #!/usr/bin/env ocaml
-(* plot_1d_time_steps.ml *)
-(**
+
+#use "topfind"
+#thread
+#require "str"
+#require "threads"
+#require "gnuplot"
+
+(** plot_1d_time_steps.ml
     Reads output of tsunami app (chapter 2) and plots the
     resulting blob position in an image file; one image file
     per time step. *)
 
-#use "topfind"
-#require "str"
-
-let input_file = "./data_1d_time_steps.txt"
+let input_file = "./data/1d_time_steps.txt"
 
 let generate_plot data_str =
   match Str.split (Str.regexp "[ ]+") data_str with
-  | time :: data -> print_endline ("time: " ^ time)
+  | time :: data -> begin
+      let open Gnuplot in
+      let gp = Gp.create () in
+      let output = Output.create (`Png (Printf.sprintf "./data/plot_1d_time_%03d.png" (int_of_string time)))
+      in
+      Gp.plot gp ~output ~use_grid:true
+        (Series.lines (List.map float_of_string data));
+      Gp.close gp
+    end
   | _            -> failwith "Invalid data"
 
 let () =
   if not (Sys.file_exists input_file) then
-    print_endline ("Could not find " ^ input_file)
+    failwith ("Could not find " ^ input_file)
   else ();
   let ic = open_in input_file in
   try
