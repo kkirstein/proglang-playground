63e9be8ad61a27896929f684ef6d04df04bb60aa
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2019-09-24 21:34:57 +0200

    implement perfect_numbers()

--- Zig/benchmark/src/perfect.zig
+++ Zig/benchmark/src/perfect.zig
@@ -16,19 +16,20 @@ pub fn is_perfect(n: u64) bool {
     return sum == n;
 }
 
+const std = @import("std");
 const List = @import("std").SinglyLinkedList;
 const heap = @import("std").heap;
 
 /// Generates perfect number up to givien limit [n]
-pub fn perfect_numbers(limit: u64) List(u64) {
+pub fn perfect_numbers(limit: u64) std.SinglyLinkedList(u64) {
     const allocator = heap.direct_allocator;
-    var res = List(u64).init();
+    var res = std.SinglyLinkedList(u64).init();
 
     var i: u64 = 0;
-    while (i <= limit) : ({ i += 1; }) {
+    while (i <= limit) : (i += 1) {
         if (is_perfect(i)) {
-            var entry = res.createNode(i, allocator);
-            //res.prepend(entry);
+            const entry = res.createNode(i, allocator) catch unreachable;
+            res.prepend(entry);
         }
     }
 
@@ -52,5 +53,11 @@ const mem = @import("std").mem;
 
 test "perfect numbers" {
     const res = perfect_numbers(1000);
-    //testing.expect(mem.eql(u64, perfect_numbers(1000), []const u64{ 6, 28, 496 }));
+    const exp = [_]u64{ 496, 28, 6, 0 };
+    var it = res.first;
+    var idx: u32 = 0;
+    while (it) |node| : (it = node.next) {
+        testing.expect(node.data == exp[idx]);
+        idx += 1;
+    }
 }
