63729b3c52500514b20e0a4d05a95fd988c55391
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2019-09-24 15:32:24 +0200

    perfect number predicate

--- Zig/benchmark/src/perfect.zig
+++ Zig/benchmark/src/perfect.zig
@@ -4,12 +4,35 @@
 /// Finding perfect numbers
 /// Predicate for perfect numbers
 pub fn is_perfect(n: u64) bool {
-    return true;
+    var i: u64 = 1;
+    var sum: u64 = 0;
+
+    while (i < n) : ({
+        i += 1;
+    }) {
+        if (n % i == 0) sum += i;
+    }
+
+    return sum == n;
 }
 
+const List = @import("std").SinglyLinkedList;
+const heap = @import("std").heap;
+
 /// Generates perfect number up to givien limit [n]
-pub fn perfect_numbers(limit: u64) []u64 {
-    return [_]u64{};
+pub fn perfect_numbers(limit: u64) List(u64) {
+    const allocator = heap.direct_allocator;
+    var res = List(u64).init();
+
+    var i: u64 = 0;
+    while (i <= limit) : ({ i += 1; }) {
+        if (is_perfect(i)) {
+            var entry = res.createNode(i, allocator);
+            //res.prepend(entry);
+        }
+    }
+
+    return res;
 }
 
 const testing = @import("std").testing;
@@ -25,6 +48,9 @@ test "is perfect" {
     testing.expect(is_perfect(28));
 }
 
+const mem = @import("std").mem;
+
 test "perfect numbers" {
-    testing.expect(perfect_numbers(1000) == [_]u64{ 6, 28, 496 });
+    const res = perfect_numbers(1000);
+    //testing.expect(mem.eql(u64, perfect_numbers(1000), []const u64{ 6, 28, 496 }));
 }
