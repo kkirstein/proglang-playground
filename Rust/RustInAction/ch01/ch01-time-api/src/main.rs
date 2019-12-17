a8abf5180dcc40f39b9b18ad34ed50bdaa2dd1cd
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2019-12-17 18:57:19 +0100

    setup RustInAction demo

---
+++ Rust/RustInAction/ch01/ch01-time-api/src/main.rs
@@ -0,0 +1,36 @@
+#![feature(plugin)]
+#![plugin(rocket_contrib)]
+
+extern crate chrono;
+extern crate rocket;
+extern crate rocket_contrib;
+extern crate serde;
+
+#[macro_use]
+extern crate serde_derive;
+
+use chrono::prelude::*;
+use rocket_contrib::Json;
+
+#[derive(Serialize)]
+struct Timestamp {
+    time: String,
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello World!"
+}
+
+#[get("/time")]
+fn time_now() -> Json<Timestamp> {
+    let now: DateTime<Utc> = Utc::now();
+    let timestamp = Timestamp {
+        time: now.to_rfc3339(),
+    };
+    Json(timestamp)
+}
+
+fn main() {
+    rocket::ignite().mount("/", [index, time_now()]).launch();
+}
