eeb78125fb52cea202f1ed417032880a9755297a
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2019-12-29 11:23:40 +0100

    use regex for string search

--- Rust/RustInAction/ch02/grep-lite/src/main.rs
+++ Rust/RustInAction/ch02/grep-lite/src/main.rs
@@ -1,12 +1,23 @@
+
+extern crate regex;
+
+use regex::Regex;
+
 fn main() {
     let search_term = "picture";
+    let re = Regex::new(search_term).unwrap();
+
     let quote = "Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of pages?"; //<1>
 
     for line in quote.lines() {
-        if line.contains(search_term) {
-            println!("{}", line);
+        match re.find(line) {
+            Some(_) => println!("{}", line),
+            None    => (),
         }
     }
+
+    // functional approach
+    //quote.lines().filter(|l| re.is_match(l)).map(|m| println!("{}", m));
 }
