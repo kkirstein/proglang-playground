4302cec23d47d67ac92432b6331c97ad0cf8e2c0
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2019-12-30 18:00:32 +0100

    implement command line interface

--- Rust/RustInAction/ch02/grep-lite/src/main.rs
+++ Rust/RustInAction/ch02/grep-lite/src/main.rs
@@ -3,6 +3,9 @@ extern crate regex;
 
 use clap::{App, Arg};
 use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     let args = App::new("grep-lite")
@@ -14,19 +17,29 @@ fn main() {
                 .takes_value(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?"; //<1>
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
 
-    for line in quote.lines() {
-        match re.find(line) {
-            Some(_) => println!("{}", line),
-            None => (),
+    for line_ in reader.lines() {
+        //let line = line_.unwrap();
+        match line_ {
+            Ok(line) => match re.find(&line) {
+                Some(_) => println!("{}", line),
+                None    => (),
+            },
+            _        => (),
         }
     }
 
