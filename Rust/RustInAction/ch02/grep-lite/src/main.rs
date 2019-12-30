538d959c58bdd4eec808fea1cf2c3c0ee303bd22
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2019-12-30 09:36:49 +0100

    command-line arg for search pattern

--- Rust/RustInAction/ch02/grep-lite/src/main.rs
+++ Rust/RustInAction/ch02/grep-lite/src/main.rs
@@ -1,11 +1,23 @@
-
+extern crate clap;
 extern crate regex;
 
+use clap::{App, Arg};
 use regex::Regex;
 
 fn main() {
-    let search_term = "picture";
-    let re = Regex::new(search_term).unwrap();
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
 
     let quote = "Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
@@ -14,7 +26,7 @@ It is the same with books. What do we seek through millions of pages?"; //<1>
     for line in quote.lines() {
         match re.find(line) {
             Some(_) => println!("{}", line),
-            None    => (),
+            None => (),
         }
     }
 
