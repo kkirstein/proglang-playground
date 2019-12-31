8e7593cd2e00104532f9b83d3954ad4d3058915d
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2019-12-31 09:25:38 +0100

    enable reading from stdin

--- Rust/RustInAction/ch02/grep-lite/src/main.rs
+++ Rust/RustInAction/ch02/grep-lite/src/main.rs
@@ -4,8 +4,22 @@ extern crate regex;
 use clap::{App, Arg};
 use regex::Regex;
 use std::fs::File;
+//use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
+
+fn process_lines<T: BufRead>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        //let line = line_.unwrap();
+        match line_ {
+            Ok(line) => match re.find(&line) {
+                Some(_) => println!("{}", line),
+                None => (),
+            },
+            _ => (),
+        }
+    }
+}
 
 fn main() {
     let args = App::new("grep-lite")
@@ -21,26 +35,23 @@ fn main() {
             Arg::with_name("input")
                 .help("File to search")
                 .takes_value(true)
-                .required(true),
+                .required(false),
         )
         .get_matches();
 
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-    let input = args.value_of("input").unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
+    let input = args.value_of("input").unwrap_or("-");
 
-    for line_ in reader.lines() {
-        //let line = line_.unwrap();
-        match line_ {
-            Ok(line) => match re.find(&line) {
-                Some(_) => println!("{}", line),
-                None    => (),
-            },
-            _        => (),
-        }
+    if input == "-" {
+        let stdin = std::io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 
     // functional approach
