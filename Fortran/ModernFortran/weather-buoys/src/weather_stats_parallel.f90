e3aaa573ad7e69551a057c7eb0183665b78eaa4a
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2020-05-18 13:39:50 +0200

    parallel weather buoy analysis

--- Fortran/ModernFortran/weather-buoys/src/weather_stats_parallel.f90
+++ Fortran/ModernFortran/weather-buoys/src/weather_stats_parallel.f90
@@ -6,33 +6,48 @@ program weather_stats
 
     use mod_array, only: denan, mean
     use mod_io, only: read_buoy
+    use mod_parallel, only: tile_indices
 
     implicit none
 
     character(len=5), allocatable :: ids(:)
     character(len=20), allocatable :: time(:)
+    real, allocatable  :: gather_max(:)[:], gather_mean(:)[:]
     real, allocatable :: wind_speed(:)
     real, allocatable :: max_wind(:), mean_wind(:)
+    integer :: is, ie, indices(2)
     integer :: i
 
     ids = ['42001', '42002', '42003', '42020', '42035',&
         '42036', '42039', '42040', '42055']
 
-    allocate(max_wind(size(ids)), mean_wind(size(ids)))
+    indices = tile_indices(size(ids))
+    is = indices(1)
+    ie = indices(2)
 
-    do i = 1, size(ids)
+    allocate(gather_max(size(ids))[*], gather_mean(size(ids))[*])
+    allocate(max_wind(is:ie), mean_wind(is:ie))
+
+    do i = is, ie
         call read_buoy('data/buoy_' // ids(i) // '.csv', time, wind_speed)
         wind_speed = denan(wind_speed)
         max_wind(i) = maxval(wind_speed)
         mean_wind(i) = mean(wind_speed)
     end do
 
-    print *, 'Maximum wind speed measured is ', maxval(max_wind),&
-        'at station ', ids(maxloc(max_wind))
-    print *, 'Highest mean wind speed is ', maxval(mean_wind),&
-        'at station ', ids(maxloc(mean_wind))
-    print *, 'Lowest mean wind speed is ', minval(mean_wind),&
-        'at station ', ids(minloc(mean_wind))
+    gather_max(is:ie)[1] = max_wind
+    gather_mean(is:ie)[1] = mean_wind
+
+    sync all
+
+    if (this_image() == 1) then
+        print *, 'Maximum wind speed measured is ', maxval(gather_max),&
+            'at station ', ids(maxloc(gather_max))
+        print *, 'Highest mean wind speed is ', maxval(gather_mean),&
+            'at station ', ids(maxloc(gather_mean))
+        print *, 'Lowest mean wind speed is ', minval(gather_mean),&
+            'at station ', ids(minloc(gather_mean))
+    end if
 
 end program weather_stats
 
