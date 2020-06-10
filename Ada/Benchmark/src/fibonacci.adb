d1d15d3d3baac1fc50d7e440cf63660d9ec04cad
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2020-06-10 10:47:15 +0200

    time different Fibonacci implementations

--- Ada/Benchmark/src/fibonacci.adb
+++ Ada/Benchmark/src/fibonacci.adb
@@ -1,3 +1,5 @@
+with Ada.Numerics.Big_Numbers.Big_Integers; use Ada.Numerics.Big_Numbers.Big_Integers;
+
 package body Fibonacci is
 
    function Fib_Iter(N : Natural) return Natural is
@@ -12,5 +14,32 @@ package body Fibonacci is
       end loop;
       return A;
    end Fib_Iter;
+
+   function Fib_Iter_Big_Int(N : Natural) return Big_Natural is
+      A   : Big_Natural := 0;
+      B   : Big_Natural := 1;
+      Tmp : Big_Natural := 0;
+   begin
+      for I in 0..N-1 loop
+         Tmp := A;
+         A   := B;
+         B   := Tmp + B;
+      end loop;
+      return A;
+   end Fib_Iter_Big_Int;
+
+   function Fib_Naive (N : Natural) return Natural is
+   begin
+      if N < 2 then return N;
+      else return Fib_Naive (N - 1) + Fib_Naive (N - 2);
+      end if;
+   end Fib_Naive;
+
+   function Fib_Recur (N : Natural) return Big_Natural is
+      -- TODO: local defs
+
+   begin
+      return 0;
+   end Fib_Recur;
 
 end Fibonacci;
--- Ada/Benchmark/src/main.adb
+++ Ada/Benchmark/src/main.adb
@@ -1,16 +1,34 @@
 
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Calendar; use Ada.Calendar;
+with Ada.Numerics.Big_Numbers.Big_Integers;
+use Ada.Numerics.Big_Numbers.Big_Integers;
 
 with Fibonacci;
 
 procedure Main is
    package Fib renames Fibonacci;
-begin
-   Put_Line("Benchmark");
-   Put_Line("=========");
 
-   Put_Line("Fibonacci Numbers");
-   Put_Line("-----------------");
-   Put_Line("Fib_Iter (35) = " & Natural'Image (Fib.Fib_Iter (35)));
+   procedure Put_Elapsed (Tic : Time; Toc : Time := Clock) is
+      begin
+      Put_Line (" elapsed time: " & Duration'Image(Toc - Tic) & "s");
+   end Put_Elapsed;
+
+   Tic : Time;
+begin
+   Put_Line ("Benchmark");
+   Put_Line ("=========");
+
+   Put_Line ("Fibonacci Numbers");
+   Put_Line ("-----------------");
+   Tic := Clock;
+   Put ("Fib_Naive (35)        = " & Natural'Image (Fib.Fib_Naive (35)));
+   Put_Elapsed (Tic);
+   Tic := Clock;
+   Put ("Fib_Iter (35)         = " & Natural'Image (Fib.Fib_Iter (35)));
+   Put_Elapsed (Tic);
+   Tic := Clock;
+   Put ("Fib_Iter_Big_Int (35) = " & To_String (Fib.Fib_Iter_Big_Int (35)));
+   Put_Elapsed (Tic);
 
 end Main;
