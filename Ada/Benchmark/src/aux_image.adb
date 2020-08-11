f0672f6db8f31b46d221e01ddd86ef0d80007f2a
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2020-08-11 17:23:06 +0200

    play around with generic function

--- Ada/Benchmark/src/aux_image.adb
+++ Ada/Benchmark/src/aux_image.adb
@@ -7,6 +7,23 @@ with Primes; use Primes;
 
 package body Aux_Image is
 
+   function Generic_Image (X : T) return String is
+      Res : Unbounded_String := Null_Unbounded_String;
+   begin
+      Res := Res & "[";
+      for E of X loop
+         Res := Res & To_String (E) & ",";
+      end loop;
+      Res := Res & "]";
+      return To_String (Res);
+   end Generic_Image;
+      
+
+   --function Img is new Generic_Image (T => Pn_Vectors.Element_Type,
+   --                                   I => Pn_Vectors.Index_Type,
+   --                                   A => Pn_Vectors.Vector,
+   --                                   To_String => Integer'Image);
+                                                                            
    function Img (X : Pn_Vectors.Vector) return String is
       Res : Unbounded_String := Null_Unbounded_String;
    begin
--- Ada/Benchmark/src/aux_image.ads
+++ Ada/Benchmark/src/aux_image.ads
@@ -1,12 +1,13 @@
 with Ada.Numerics.Big_Numbers.Big_Integers;
 use Ada.Numerics.Big_Numbers.Big_Integers;
+with Ada.Containers.Vectors;
 
 with Fibonacci; use Fibonacci;
 with Perfect_Number; use Perfect_Number;
 with Primes; use Primes;
 
 package Aux_Image is
-
+  
    function Img (X : Natural) return String renames Natural'Image;
 
    function Img (X : Big_Natural) return String renames Big_Natural_Image;
@@ -17,4 +18,12 @@ package Aux_Image is
 
    function Img (X : Big_Prime_Vectors.Vector) return String;
 
+private
+   generic
+      type T is abstract new Ada.Containers.Vectors.Vector;
+      --type I is (<>);
+      --type A is array (I) of T;
+      --with function To_String (X : E) return String;
+   function Generic_Image (X : T) return String;
+     
 end Aux_Image;
