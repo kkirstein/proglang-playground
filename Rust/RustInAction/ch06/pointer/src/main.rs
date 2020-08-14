899ae887b962558d38a6e8764921c3fe548c7f00
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2020-08-14 15:25:53 +0200

    print info on different pointer types

--- Rust/RustInAction/ch06/pointer/src/main.rs
+++ Rust/RustInAction/ch06/pointer/src/main.rs
@@ -1,12 +1,40 @@
+use std::mem::size_of;
+
 // some data
 static B: [u8; 10] = [99, 97, 114, 114, 121, 116, 111, 119, 101, 108];
 static C: [u8; 11] = [116, 104, 97, 110, 107, 115, 102, 105, 115, 104, 0];
 
-fn main() {
+// print_info
+fn print_info_ptr<T: std::fmt::Pointer>(desc: &str, x: T) {
+    println!("{}:", desc);
+    println!(" location:  {:p}", &x);
+    println!(" size:      {:?} bytes", size_of::<T>());
+    println!(" points to: {:p}", x);
+}
 
+fn print_info<T: std::fmt::Debug>(desc: &str, x: T) {
+    println!("{}:", desc);
+    println!(" location:  {:p}", &x);
+    println!(" size:      {:?} bytes", size_of::<T>());
+    println!(" value:     {:?}", x);
+}
+
+fn main() {
     let a = 42;
     let b = &B;
-    let c = &C;
+    let c = Box::new(C);
 
-    println!("a: {:?}, b: {:p}, c: {:p}", a, b, c);
+    print_info("a (an unsigned integer)", a);
+    println!();
+
+    print_info_ptr("b (a reference to B)", b);
+    println!();
+
+    print_info_ptr("c (a 'box' for C):", c);
+    println!();
+
+    print_info("B (an array of 10 bytes)", B);
+    println!();
+
+    print_info("C (an array of 11 bytes)", C);
 }
