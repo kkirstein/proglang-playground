6662d7935a0555a975eb10fe9f3c3215c296a74c
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2020-12-30 15:56:13 +0100

    analyse SVR scores

--- Python/EnsembleLearning/chapter_01/fit_complex.py
+++ Python/EnsembleLearning/chapter_01/fit_complex.py
@@ -16,6 +16,7 @@ import seaborn as sns
 from sklearn.datasets import load_boston
 from sklearn.preprocessing import StandardScaler
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
 from sklearn.model_selection import ShuffleSplit, validation_curve
 
 # load & scale dataset
@@ -39,7 +40,7 @@ trn_scores, tst_scores = validation_curve(
 
 mean_trn_scores = np.mean(trn_scores, axis=1)
 mean_tst_scores = np.mean(tst_scores, axis=1)
-df = pd.concat([
+dtree_scores = pd.concat([
     pd.DataFrame({
         'max_depth': dtree_max_depth,
         'score': mean_trn_scores,
@@ -56,7 +57,43 @@ df = pd.concat([
 # print(df)
 sns.set_style('darkgrid')
 dtree_plot = sns.relplot(
-    data=df,
+    data=dtree_scores,
     x='max_depth', y='score', hue='subset'
-)
+).set_titles('dtree scores')
 dtree_plot.savefig('dtree_scores.png')
+
+# regression by support vector machine
+svr_model = SVR(degree=3)
+svr_loss_factor = np.logspace(-2, 4, 7)
+
+trn_scores, tst_scores = validation_curve(
+    svr_model,
+    X, y.ravel(),
+    param_name='C',
+    param_range=svr_loss_factor,
+    cv=subsets,
+    scoring='r2')
+
+mean_trn_scores = np.mean(trn_scores, axis=1)
+mean_tst_scores = np.mean(tst_scores, axis=1)
+
+svr_scores = pd.concat([
+    pd.DataFrame({
+        'loss_factor': svr_loss_factor,
+        'score': mean_trn_scores,
+        'subset': 'train'
+    }),
+    pd.DataFrame({
+        'loss_factor': svr_loss_factor,
+        'score': mean_tst_scores,
+        'subset': 'test'
+    })
+])
+
+# show dtree scores
+sns.set_style('darkgrid')
+svr_plot = sns.relplot(
+    data=svr_scores,
+    x='loss_factor', y='score', hue='subset'
+).set_titles('SVR scores')
+dtree_plot.savefig('svr_scores.png')
