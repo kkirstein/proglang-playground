319646c131b25067c45d6d06137a30e035aa4702
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2021-05-18 17:12:41 +0200

    define windows target to build correct c runtime

--- Zig/benchmark/build.zig
+++ Zig/benchmark/build.zig
@@ -2,12 +2,22 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
+    const windows = b.option(bool, "windows", "create windows build") orelse false;
+
     const exe = b.addExecutable("benchmark", "src/main.zig");
     exe.setBuildMode(mode);
     exe.linkLibC();
+    exe.addIncludeDir("./src");
+
+    if (windows) {
+        exe.setTarget(.{
+            .cpu_arch = .x86_64,
+            .os_tag = .windows,
+            .abi = .gnu,
+        });
+    }
 
     const run_cmd = exe.run();
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
