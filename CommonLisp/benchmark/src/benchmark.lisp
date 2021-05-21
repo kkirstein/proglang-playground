b8786d17317f511202e6eb240e99c57b16e63238
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2021-05-21 18:12:13 +0200

    format console output

--- CommonLisp/benchmark/src/benchmark.lisp
+++ CommonLisp/benchmark/src/benchmark.lisp
@@ -7,39 +7,53 @@
 (defun main ()
   (progn
 
+    (format t "Benchmark~%")
+    (format t "=========~%")
     ;;
     ;; time different versions of Fibanocci calculations
     ;;
+    (format t "Fibonacci series:~%")
+    (format t "-----------------~%")
+    
     (format t "Fibonacci series (double recursive approach):~%")
-    (time (fibonacci:fibonacci-2 35))
+    (time (format t "(fib-naive 35) = ~a~%" (fibonacci:fibonacci-2 35)))
     (format t "Fibonacci series (linear recursive approach):~%")
-    (time (fibonacci:fibonacci 1000))
+    (time (format t "(fib 10000) = ~a~%" (fibonacci:fibonacci 10000)))
     (format t "~%")
+    
 
     ;;
     ;; time computation of perfect numbers
     ;;
     (format t "Perfect numbers:~%")
-    (time (perfect-numbers:make-perfect-numbers 10000))
+    (format t "----------------~%")
+    (time (format t "(perfect-numbers 10000) = ~a~%" (perfect-numbers:make-perfect-numbers 10000)))
     (format t "~%")
 
     ;;
     ;; time computation of Mandelbrot sets
     ;;
-    (defconstant bw-file "mandelbrot_bw.pgm")
-    (defconstant rgb-file "mandelbrot_rgb.pgm")
+    (defparameter bw-file "mandelbrot_bw.pgm")
+    (defparameter rgb-file "mandelbrot_rgb.ppm")
     (format t "Mandelbrot sets:~%")
+    (format t "----------------~%")
     (time (mandelbrot:write-pgm-bw bw-file 180 120 -0.5 0.0 (/ 4.0 180)))
     (time (mandelbrot:write-pgm-bw bw-file 640 480 -0.5 0.0 (/ 4.0 640)))
     (time (mandelbrot:write-pgm-rgb rgb-file 640 480 -0.5 0.0 (/ 4.0 640)))
     (format t "~%")
 
     ;;
+    ;; Monte-Carlo simulations
+    ;;
+    ;(format t "Monte-Carlo simulations:~%")
+    ;(format t "------------------------~%")
+    
+    ;;
     ;; clean-up and finish
     ;;
-    (format t "DONE~%")
+    (format t "DONE~%"))
 
-    :DONE))
+  :DONE)
 
 ;;
 ;; call main entry point
