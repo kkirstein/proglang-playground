17e0a7bc8fd5d6644227ec6d59377d5e2807a387
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2021-05-21 20:00:02 +0200

    autoformat source

--- FSharp/Benchmark/Tasks/Perfectnumber.fs
+++ FSharp/Benchmark/Tasks/Perfectnumber.fs
@@ -7,30 +7,40 @@ module Perfectnumber =
     // predicate to check whether a number is perfect
     let isPerfect n =
         let rec loop i sum =
-            if n = i then sum = n else
-            if n % i = 0 then loop (i + 1) (sum + i) else loop (i + 1) sum
-        in
-        if n > 0 then loop 1 0 else failwith "n must > 0"
+            if n = i then
+                sum = n
+            else if n % i = 0 then
+                loop (i + 1) (sum + i)
+            else
+                loop (i + 1) sum
+
+        if n > 0 then
+            loop 1 0
+        else
+            failwith "n must > 0"
 
 
     // generate a list of perfect numbers until given limit
     let perfectNumbers n =
         let rec loop i =
-            if i = n then []
+            if i = n then
+                []
+            else if isPerfect i then
+                i :: loop (i + 1)
             else
-                if isPerfect i then i :: loop (i + 1) else loop (i + 1)
-        in
+                loop (i + 1)
+
         loop 1
 
     let rec range start stop =
-        if start < stop then start :: (range (start + 1) stop) else []
+        if start < stop then
+            start :: (range (start + 1) stop)
+        else
+            []
 
 
     // async variant of perfect number generation
-    let isPerfectAsync n =
-        async {
-           return isPerfect n, n
-        }
+    let isPerfectAsync n = async { return isPerfect n, n }
 
     let perfectNumbersAsync n =
         range 1 n
@@ -40,4 +50,3 @@ module Perfectnumber =
         |> Seq.filter fst
         |> Seq.map snd
         |> Seq.toList
-
