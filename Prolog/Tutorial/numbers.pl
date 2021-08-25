61d2544ef389c4ea5208007af3a6c54b221b9261
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2021-08-25 17:00:52 +0200

    even/odd numbers

--- Prolog/Tutorial/numbers.pl
+++ Prolog/Tutorial/numbers.pl
@@ -1,4 +1,15 @@
-gerade(2).
-ungerade(1).
+% vim: set ft=prolog:
+%
+% check for numeric properties, like even/odd
+
+test(1, odd).
+test(2, even).
+
+test(X, Z) :-
+	test(Y, Z),
+	X is Y + 2 .
+
+%is_even(X) :- Z = even, test(X, Z).
+%is_odd(X) :- Z = odd, test(X, Z).
 
 
