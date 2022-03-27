443f3f3de87d8485fed81e612f241defcb4ed999
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2022-03-27 21:18:21 +0200

    overload tile_indices

--- Fortran/ModernFortran/tsunami/mod_parallel.f90
+++ Fortran/ModernFortran/tsunami/mod_parallel.f90
@@ -2,14 +2,19 @@ module mod_parallel
 
     ! Module that provides convinience procedures for parallel execution
 
+    use iso_fortran_env, only: int32
+
     implicit none
 
     private
-    public :: tile_indices_1d, tile_neighbors
+    public :: tile_indices, tile_neighbors
 
+    interface tile_indices
+        module procedure tile_indices, tile_indices_1d, tile_indices_2d
+    end interface tile_indices
 contains
 
-    pure function tile_indices_1d(dims) result(tile_indices)
+    pure function tile_indices(dims)
         integer, intent(in) :: dims
         integer :: tile_indices(2)
         integer :: offset, tile_size
@@ -27,9 +32,38 @@ contains
             tile_indices(2) = tile_indices(2) + this_image() - offset
         end if
 
+    end function tile_indices
+
+
+    pure function tile_indices_1d(dims, i, n) result(indices)
+        integer(int32), intent(in) :: dims, i, n
+        integer(int32) :: indices(2)
+        integer(int32) :: offset, tile_size
+
+        ! first guess if dims is divisible by num_images
+        tile_size = dims / n
+
+        indices(1) = (i - 1) * tile_size + 1
+        indices(2) = indices(1) + tile_size - 1
+
+        ! deal with possible reminder
+        offset = num_images() - mod(dims, num_images())
+        if (i > offset) then
+            indices(1) = indices(1) + i  - offset -1
+            indices(2) = indices(2) + i - offset
+        end if
+
     end function tile_indices_1d
 
-    ! TODO: tile_indices_2d()
+
+    pure function tile_indices_2d(dims) result(indices)
+        integer, intent(in) :: dims(2)
+        integer(int32) :: indices(4)
+
+        ! TODO: tile_indices_2d()
+        indices = 0
+    end function tile_indices_2d
+
 
     pure function tile_neighbors()
         integer :: tile_neighbors(2)
