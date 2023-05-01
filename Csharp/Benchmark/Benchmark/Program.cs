5588b09c632f4baab4f3e6b3426cfaf8226aae25
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2023-05-01 16:37:29 +0200

    setup Benchmark project

---
+++ Csharp/Benchmark/Benchmark/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Benchmark
+{
+    internal class Program
+    {
+        static void Main(string[] _args)
+        {
+            Console.WriteLine("Benchmark");
+            Console.WriteLine("=========");
+            Console.WriteLine();
+
+
+            Console.WriteLine("Fibonacci Numbers");
+            Console.WriteLine("-----------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Perfect Numbers");
+            Console.WriteLine("---------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Prime Numbers");
+            Console.WriteLine("-------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Mandelbrot Set");
+            Console.WriteLine("--------------");
+        }
+    }
+}
