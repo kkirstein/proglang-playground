ce264441324481368a5dea19bade644cbf5c3e2b
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2023-11-27 16:31:42 +0100

    automatically determine asd file path

--- CommonLisp/benchmark/build.lisp
+++ CommonLisp/benchmark/build.lisp
@@ -1,4 +1,11 @@
-(asdf:load-asd #P"C:/Users/kayuwe.kirstein/Workspaces/Misc/proglang-playground/CommonLisp/benchmark/benchmark.asd")
+;; get some filesystem utilities
+(ql:quickload "uiop")
+
+;; get path to asdf system definition
+(defparameter *asdf-file* (uiop:merge-pathnames* (uiop/os:getcwd) "benchmark.asd"))
+
+;; load system and dump core
+(asdf:load-asd *asdf-file*)
 (ql:quickload "benchmark")
 (sb-ext:save-lisp-and-die
   "benchmark.core"
