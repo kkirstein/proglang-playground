7169a1fa31355a81f58473ed0f75f2750d78abf0
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2024-09-28 10:10:01 +0200

    setup alire project

--- Ada/Benchmark/src/benchmark.adb
+++ Ada/Benchmark/src/benchmark.adb
@@ -1,5 +1,4 @@
-
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Text_IO;  use Ada.Text_IO;
 with Ada.Calendar; use Ada.Calendar;
 with Ada.Numerics.Big_Numbers.Big_Integers;
 use Ada.Numerics.Big_Numbers.Big_Integers;
@@ -9,13 +8,13 @@ with Perfect_Number;
 with Primes;
 with Aux_Image; use Aux_Image;
 
-procedure Main is
+procedure Benchmark is
    package Fib renames Fibonacci;
    package Pn renames Perfect_Number;
 
    procedure Put_Elapsed (Tic : Time; Toc : Time := Clock) is
    begin
-      Put_Line (" elapsed time: " & Duration'Image(Toc - Tic) & "s");
+      Put_Line (" elapsed time: " & Duration'Image (Toc - Tic) & "s");
    end Put_Elapsed;
 
    Tic : Time;
@@ -30,28 +29,32 @@ begin
    Put ("Fib_Naive (35)   = " & Img (Fib.Fib_Naive (35)));
    Put_Elapsed (Tic);
    Tic := Clock;
-   Put ("Fib_Iter (1000)  = " & Img (Fib.Fib_Iter (1000)));
+   Put ("Fib_Iter (1000)  = " & Img (Fib.Fib_Iter (1_000)));
    Put_Elapsed (Tic);
    Tic := Clock;
-   Put ("Fib_Recur (1000) = " & Img (Fib.Fib_Recur (1000)));
+   Put ("Fib_Recur (1000) = " & Img (Fib.Fib_Recur (1_000)));
    Put_Elapsed (Tic);
    New_Line;
 
    Put_Line ("Perfect Numbers");
    Put_Line ("---------------");
    Tic := Clock;
-   Put ("Perfect_Numbers (10000) = " & Img (Pn.Get_Perfect_Numbers (10000)));
+   Put ("Perfect_Numbers (10000) = " & Img (Pn.Get_Perfect_Numbers (10_000)));
    Put_Elapsed (Tic);
-   New_line;
+   New_Line;
 
    Put_Line ("Prime Numbers");
    Put_Line ("-------------");
    Tic := Clock;
-   Put ("Get_Primes (10000): (" & Img (Primes.Get_Primes (Integer (10_000))) & ")");
+   Put
+     ("Get_Primes (10000): (" & Img (Primes.Get_Primes (Integer (10_000))) &
+      ")");
    Put_Elapsed (Tic);
    Tic := Clock;
-   Put ("Get_Primes (10000): (" & Img (Primes.Get_Primes (To_Big_Integer (10_000))) & ")");
+   Put
+     ("Get_Primes (10000): (" &
+      Img (Primes.Get_Primes (To_Big_Integer (10_000))) & ")");
    Put_Elapsed (Tic);
    New_Line;
 
-end Main;
+end Benchmark;
