b6b17c5674cdde71ed38be088c990d76ffce26a1
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2025-03-12 16:07:59 +0100

    update build.zig for zig 0.14

--- Zig/benchmark/build.zig
+++ Zig/benchmark/build.zig
@@ -1,27 +1,35 @@
-const Builder = @import("std").build.Builder;
+const std = @import("std");
 
-pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
-    const windows = b.option(bool, "windows", "create windows build") orelse false;
+pub fn build(b: *std.Build) void {
 
-    const exe = b.addExecutable("benchmark", "src/main.zig");
-    exe.addCSourceFile("src/stb_image_impl.c", &[_][]const u8{"-std=c99"});
-    exe.setBuildMode(mode);
+    // build options (left as standard so far)
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    //const mode = b.standardReleaseOptions();
+    //const windows = b.option(bool, "windows", "create windows build") orelse false;
+
+    // executable artifact
+    const exe = b.addExecutable(.{ .name = "benchmark", .root_source_file = b.path("./src/main.zig"), .target = target, .optimize = optimize });
+    //exe.addCSourceFile("src/stb_image_impl.c", &[_][]const u8{"-std=c99"});
+    exe.addCSourceFiles(.{ .root = b.path("./src"), .files = &.{"stb_image_impl.c"} });
+    exe.addIncludePath(b.path("./src"));
+
     exe.linkLibC();
-    exe.addIncludeDir("./src");
+    //exe.addIncludeDir("./src");
 
-    if (windows) {
-        exe.setTarget(.{
-            .cpu_arch = .x86_64,
-            .os_tag = .windows,
-            .abi = .gnu,
-        });
-    }
+    //if (windows) {
+    //    exe.setTarget(.{
+    //        .cpu_arch = .x86_64,
+    //        .os_tag = .windows,
+    //        .abi = .gnu,
+    //    });
+    //}
 
-    const run_cmd = exe.run();
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
+
+    // add run step
+    const run_exe = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run benchmark application");
+    run_step.dependOn(&run_exe.step);
 }
