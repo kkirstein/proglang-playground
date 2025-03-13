7d12ca38d8aac171c988be5e704315eb869838e6
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2025-03-13 11:39:26 +0100

    fix Random namespace

--- Zig/benchmark/src/montecarlo.zig
+++ Zig/benchmark/src/montecarlo.zig
@@ -10,7 +10,7 @@ const Result = struct { success: usize, failed: usize };
 
 /// type of generator for a stimulus of type T of a Monte-Carlo run
 fn Generator(comptime T: type) type {
-    return fn (*std.rand.Random) T;
+    return fn (*const std.Random) T;
 }
 
 /// type of Monte-Carlo simulation evaluator
@@ -19,7 +19,7 @@ fn Evaluator(comptime T: type) type {
 }
 
 /// runs a Monte-Carlo simulation with given number of runs
-fn runner(comptime T: type, gen: Generator(T), eval: Evaluator(T), rand: *std.rand.Random, n: usize) Result {
+fn runner(comptime T: type, gen: Generator(T), eval: Evaluator(T), rand: *const std.Random, n: usize) Result {
     var res = Result{ .success = 0, .failed = 0 };
     var i: usize = 0;
 
@@ -36,7 +36,7 @@ fn runner(comptime T: type, gen: Generator(T), eval: Evaluator(T), rand: *std.ra
 }
 
 /// generate random distance for pi simulation
-fn piGen(rnd: *std.rand.Random) f64 {
+fn piGen(rnd: *const std.Random) f64 {
     const x = 2.0 * rnd.float(f64) - 1.0;
     const y = 2.0 * rnd.float(f64) - 1.0;
 
@@ -54,19 +54,19 @@ fn piEval(val: f64) bool {
 pub fn simulatePi(count: usize) f64 {
 
     // init random generator
-    var prng = std.rand.DefaultPrng.init(0);
-    const rnd = &prng.random;
+    var prng = std.Random.DefaultPrng.init(0);
+    const rnd = &prng.random();
 
     const stat = runner(f64, piGen, piEval, rnd, count);
 
-    const res = 4 * @as(f64, stat.success) / @as(f64, count);
+    const res = 4 * @as(f64, @floatFromInt(stat.success)) / @as(f64, @floatFromInt(count));
     return res;
 }
 
 // testing
 const testing = std.testing;
 
-fn simpleSampler(rnd: *std.rand.Random) u8 {
+fn simpleSampler(rnd: *const std.Random) u8 {
     return rnd.intRangeAtMost(u8, 0, 1);
 }
 fn trueEval(val: u8) bool {
@@ -74,8 +74,8 @@ fn trueEval(val: u8) bool {
 }
 
 test "no samples" {
-    var prng = std.rand.DefaultPrng.init(0);
-    const rnd = &prng.random;
+    var prng = std.Random.DefaultPrng.init(0);
+    const rnd = &prng.random();
     const res = runner(u8, simpleSampler, trueEval, rnd, 0);
 
     try testing.expect(res.success == 0);
@@ -83,8 +83,8 @@ test "no samples" {
 }
 
 test "100 samples" {
-    var prng = std.rand.DefaultPrng.init(0);
-    const rnd = &prng.random;
+    var prng = std.Random.DefaultPrng.init(0);
+    const rnd = &prng.random();
     const res = runner(u8, simpleSampler, trueEval, rnd, 100);
 
     try testing.expect(res.success + res.failed == 100);
@@ -94,5 +94,5 @@ test "calulate Pi" {
     const count = 1_000_000;
     const res = simulatePi(count);
 
-    try testing.expect(std.math.absFloat(res - std.math.pi) < 1e-4);
+    try testing.expect(@abs(res - std.math.pi) < 1e-3);
 }
