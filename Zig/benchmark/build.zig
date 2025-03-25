2973aed9042327fbe84c6ee26fe8c4d1fe0f4f97
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2025-03-25 15:13:57 +0100

    setup test step

--- Zig/benchmark/build.zig
+++ Zig/benchmark/build.zig
@@ -6,25 +6,11 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    //const mode = b.standardReleaseOptions();
-    //const windows = b.option(bool, "windows", "create windows build") orelse false;
-
     // executable artifact
     const exe = b.addExecutable(.{ .name = "benchmark", .root_source_file = b.path("./src/main.zig"), .target = target, .optimize = optimize });
-    //exe.addCSourceFile("src/stb_image_impl.c", &[_][]const u8{"-std=c99"});
     exe.addCSourceFiles(.{ .root = b.path("./src"), .files = &.{"stb_image_impl.c"}, .flags = &.{"-std=c99"} });
     exe.addIncludePath(b.path("./src"));
-
     exe.linkLibC();
-    //exe.addIncludeDir("./src");
-
-    //if (windows) {
-    //    exe.setTarget(.{
-    //        .cpu_arch = .x86_64,
-    //        .os_tag = .windows,
-    //        .abi = .gnu,
-    //    });
-    //}
 
     b.installArtifact(exe);
 
@@ -32,4 +18,12 @@ pub fn build(b: *std.Build) void {
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run benchmark application");
     run_step.dependOn(&run_exe.step);
+
+    // test artifact
+    const tests_exe = b.addTest(.{ .root_source_file = b.path("./src/main.zig"), .target = target, .optimize = optimize });
+
+    // ad test step
+    const run_tests = b.addRunArtifact(tests_exe);
+    const test_step = b.step("test", "Run all tests");
+    test_step.dependOn(&run_tests.step);
 }
