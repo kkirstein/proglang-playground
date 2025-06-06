a392f5f80bebe791ba0d009f0237cf21c6ae27e1
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2025-06-06 14:37:29 +0200

    remove async implementation

--- JS/Benchmark/fib.js
+++ JS/Benchmark/fib.js
@@ -7,25 +7,34 @@
 var BigInt = require('BigInt');
 
 // naive & efficient (BigInt) implementation
-function _fib_naive (n) {
-	return (n < 2)? n : arguments.callee(n-1) + arguments.callee(n-2);
+function fib_naive(n) {
+	//return (n < 2)? n : arguments.callee(n-1) + arguments.callee(n-2);
+	return (n < 2) ? n : fib_naive(n - 1) + fib_naive(n - 2);
 }
 
-function fib_naive (n) {
-	return new Promise(function (resolve, reject) {
-		let result = _fib_naive(n);
-		resolve(result);
-	})
+//function fib_naive (n) {
+//	return new Promise(function (resolve, reject) {
+//		let result = _fib_naive(n);
+//		resolve(result);
+//	})
+//}
+
+function _fib_aux(n, a, b) {
+	return (n === 0) ? a : _fib_aux(n - 1, b, BigInt.add(a, b));
 }
 
-function fib (n) {
-	return new Promise((resolve, reject) => {
-		const res = function(n, a, b) {
-			return (n === 0)? a : arguments.callee(n-1, b, BigInt.add(a, b));
-		}(n, new BigInt.int2bigInt(0, 32), new BigInt.int2bigInt(1, 32));
-		resolve(res);
-	});
+function fib(n) {
+	return _fib_aux(n, new BigInt.int2bigInt(0, 32), new BigInt.int2bigInt(1, 32));
 }
+
+//function fib (n) {
+//	return new Promise((resolve, reject) => {
+//		const res = function(n, a, b) {
+//			return (n === 0)? a : arguments.callee(n-1, b, BigInt.add(a, b));
+//		}(n, new BigInt.int2bigInt(0, 32), new BigInt.int2bigInt(1, 32));
+//		resolve(res);
+//	});
+//}
 
 // exports
 exports.fib_naive = fib_naive;
