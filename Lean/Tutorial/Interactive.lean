35d18327342bdd43c22c245eee1c9d74a5a985ad
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2025-12-18 14:18:05 +0100

    deal with product and sum types

--- Lean/Tutorial/Interactive.lean
+++ Lean/Tutorial/Interactive.lean
@@ -1,4 +1,4 @@
--- Tutorial
+-- Tutorial [Functional Programming in Lean](https://lean-lang.org/functional_programming_in_lean/)
 -- 1.1 Evaluating Expressiong
 
 #eval 42 + 19
@@ -47,7 +47,7 @@ def joinStringsWith (sep : String) (before : String) (after : String) :=
 #eval joinStringsWith ", " "Hello" "World"
 #check joinStringsWith ", "
 
-def volume (height : Nat) (width : Nat) (depth : Nat) := height * width * depth
+--def volume (height : Nat) (width : Nat) (depth : Nat) := height * width * depth
 
 -- 1.3.2 Defining Types
 
@@ -58,3 +58,51 @@ def thirtyEight : NaturalNumber := (38 : Nat)
 abbrev N : Type := Nat
 
 def ThirtyTwo : N := 32
+
+-- 1.4 Structures
+
+structure Point where
+  x : Float
+  y : Float
+
+structure Point3D where
+  x : Float
+  y : Float
+  z : Float
+
+structure RectangularPrism where
+  height : Float
+  width : Float
+  depth : Float
+
+def volume (x : RectangularPrism) : Float := x.height * x.width * x.depth
+
+structure Segment where
+  p1 : Point
+  p2 : Point
+
+structure Segment3D where
+  p1 : Point3D
+  p2 : Point3D
+
+def length (s : Segment) : Float :=
+  Float.sqrt ((s.p1.x - s.p2.x) ^2 + (s.p1.y - s.p2.y) ^2)
+
+structure Hamster where
+  name : String
+  fluffy : Bool
+
+structure Book where
+  makeBook ::
+  title : String
+  author : String
+  price : Float
+
+-- 1.5 Datatypes and Pattrns
+
+-- 1.5.2 Recursive Functions
+
+def even (n : Nat) : Bool :=
+  match n with
+  | Nat.zero => true
+  | Nat.succ k => not (even k)
