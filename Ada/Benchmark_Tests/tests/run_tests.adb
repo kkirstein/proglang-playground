bc38b3e7b7a00a3b85c4fa280800d4f9687ebc7e
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2026-02-27 13:44:26 +0100

    fix Test_Suite package name

--- Ada/Benchmark_Tests/tests/run_tests.adb
+++ Ada/Benchmark_Tests/tests/run_tests.adb
@@ -1,9 +1,9 @@
-with My_Suite;
+with Test_Suite;
 with AUnit.Run;
 with AUnit.Reporter.Text;
 
 procedure Run_Tests is
-   procedure Runner is new AUnit.Run.Test_Runner (My_Suite.Suite);
+   procedure Runner is new AUnit.Run.Test_Runner (Test_Suite.Suite);
    Reporter : AUnit.Reporter.Text.Text_Reporter;
 begin
    Runner (Reporter);
